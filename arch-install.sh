491a87496d77314e31fe6b38104fa8d3f8687402
Aidan <aidan@archdell.localdomain>
2015-09-22 14:14:33 +0100

    V1

---
+++ arch-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# init
+function pause(){
+   read -p "$*"
+}
+
+# main
+pacstrap /mnt base base-devel
+genfstab -U /mnt > /mnt/etc/fstab
+cat /mnt/etc/fstab
+pause 'Press [Enter] key if fstab is correct...'
+sed -i '/^#.* en_GB.UTF-8 /s/^#//' /etc/locale.gen
+locale-gen
