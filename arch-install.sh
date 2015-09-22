82441ab32b84eb93c12a414b7f0503d150e50a26
Timoral <atimson123@gmail.com>
2015-09-22 14:38:54 +0100

    V1

--- arch-install.sh
+++ arch-install.sh
@@ -9,5 +9,6 @@ pacstrap /mnt base base-devel
 genfstab -U /mnt > /mnt/etc/fstab
 cat /mnt/etc/fstab
 pause 'Press [Enter] key if fstab is correct...'
+arch-chroot /mnt /bin/bash
 sed -i '/^#.* en_GB.UTF-8 /s/^#//' /etc/locale.gen
 locale-gen
